921e10d85b4c82531cb7556b1ab0e5bc76e10876
PlanchonB <79264622+PlanchonB@users.noreply.github.com>
2024-09-10 18:03:35 +0200

    fix: fyne does not support borderless window

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Stinote is a simple sticky note application")
-
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Stinote")
-	myWindow.Resize(fyne.NewSize(320, 285))
-
-	textArea := widget.NewMultiLineEntry()
-	textArea.Wrapping = fyne.TextWrapWord
-	textArea.TextStyle = fyne.TextStyle{Monospace: true}
-
-	myWindow.SetContent(container.NewVBox(textArea))
-
-	myWindow.ShowAndRun()
 }
