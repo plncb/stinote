a1f0ba9f38def2e222a6732189aeaa063edf6881
PlanchonB <79264622+PlanchonB@users.noreply.github.com>
2024-09-10 15:15:39 +0200

    feat: fyne app working

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 	fmt.Println("Stinote is a simple sticky note application")
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Stinote")
+	myWindow.Resize(fyne.NewSize(320, 285))
+
+	textArea := widget.NewMultiLineEntry()
+	textArea.Wrapping = fyne.TextWrapWord
+	textArea.TextStyle = fyne.TextStyle{Monospace: true}
+
+	myWindow.SetContent(container.NewVBox(textArea))
+
+	myWindow.ShowAndRun()
 }
