46ee6d6b8e93412adf3118d53aa1b31114f2fa26
PlanchonB <79264622+plncb@users.noreply.github.com>
2024-09-13 17:27:44 +0200

    fix: handle dual screen at startup

--- window/windowState.go
+++ window/windowState.go
@@ -3,10 +3,12 @@ package window
 import (
 	"log"
 	"runtime"
+	"stinote/consts"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/driver"
 	"github.com/jezek/xgb"
+	"github.com/jezek/xgb/randr"
 	"github.com/jezek/xgb/xproto"
 )
 
@@ -85,19 +87,70 @@ func SetWindowAlwaysOnTop(win fyne.Window) {
 	})
 }
 
-// TODO : Fix for dualscreen
 func SetWindowOnTopRightCorner(win fyne.Window) {
-	//runX11Operation(win, func(x11Context driver.X11WindowContext, x *xgb.Conn, screen *xproto.ScreenInfo) {
-	//	x11Window := x11Context.WindowHandle
+	runX11Operation(win, func(x11Context driver.X11WindowContext, x *xgb.Conn, screen *xproto.ScreenInfo) {
+		x11Window := x11Context.WindowHandle
 
-	//	// Calculate top-right position
-	//	width := int16(screen.WidthInPixels) - consts.WindowWidth // Screen width minus window width
-	//	height := int16(0)                                        // Top position
+		// Initialize RandR extension
+		err := randr.Init(x)
+		if err != nil {
+			log.Fatalf("Failed to initialize RandR: %v", err)
+		}
 
-	//	// Move the window to the top-right corner
-	//	err := xproto.ConfigureWindowChecked(x, xproto.Window(x11Window), xproto.ConfigWindowX|xproto.ConfigWindowY, []uint32{uint32(width), uint32(height)}).Check()
-	//	if err != nil {
-	//		log.Fatalf("Failed to move window: %v", err)
-	//	}
-	//})
+		// Get monitor informations
+		resources, err := randr.GetScreenResourcesCurrent(x, screen.Root).Reply()
+		if err != nil {
+			log.Fatalf("Failed to get RandR screen resources: %v", err)
+		}
+
+		// Get focused window
+		focusedWindowReply, err := xproto.GetInputFocus(x).Reply()
+		if err != nil {
+			log.Fatalf("Failed to get focused window: %v", err)
+		}
+		focusedWindow := focusedWindowReply.Focus
+
+		// Iterate through monitor configurations
+		for _, ctrc := range resources.Crtcs {
+			crtcInfo, err := randr.GetCrtcInfo(x, ctrc, 0).Reply()
+			if err != nil {
+				log.Fatalf("Failed to get crtc info: %v", err)
+			}
+
+			// Only use active monitor
+			if crtcInfo.Width != 0 && crtcInfo.Height != 0 {
+				// Only use focused window
+				if windowInMonitor(focusedWindow, x, crtcInfo) {
+					width := int16(crtcInfo.X + int16(crtcInfo.Width) - consts.WindowWidth)
+					height := int16(crtcInfo.Y)
+
+					// Move the window to the top-right corner
+					err = xproto.ConfigureWindowChecked(
+						x,
+						xproto.Window(x11Window),
+						xproto.ConfigWindowX|xproto.ConfigWindowY,
+						[]uint32{uint32(width), uint32(height)},
+					).Check()
+					if err != nil {
+						log.Fatalf("Failed to move window: %v", err)
+					}
+				}
+			}
+		}
+
+	})
+}
+
+// Helper function to check if a window is within a monitor's CRTC
+func windowInMonitor(window xproto.Window, x *xgb.Conn, crtcInfo *randr.GetCrtcInfoReply) bool {
+	windowGeom, err := xproto.GetGeometry(x, xproto.Drawable(window)).Reply()
+	if err != nil {
+		log.Fatalf("Failed to get window geometry: %v", err)
+	}
+
+	// Check if the window is within the bounds of the monitor (crtc)
+	return int16(windowGeom.X) >= crtcInfo.X &&
+		int16(windowGeom.X) <= crtcInfo.X+int16(crtcInfo.Width) &&
+		int16(windowGeom.Y) >= crtcInfo.Y &&
+		int16(windowGeom.Y) <= crtcInfo.Y+int16(crtcInfo.Height)
 }
